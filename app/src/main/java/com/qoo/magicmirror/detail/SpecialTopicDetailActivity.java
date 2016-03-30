a54d992280b9ce4e69356cac9081d63481f69b08
杨琨 <503136522@qq.com>
2016-03-30 14:22:39 +0800

    三方

--- app/src/main/java/com/qoo/magicmirror/detail/SpecialTopicDetailActivity.java
+++ app/src/main/java/com/qoo/magicmirror/detail/SpecialTopicDetailActivity.java
@@ -11,6 +11,9 @@ import com.qoo.magicmirror.R;
 
 import java.util.ArrayList;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 /**
  * Created by Giraffe on 16/3/29.
  */
@@ -81,6 +84,28 @@ public class SpecialTopicDetailActivity extends AppCompatActivity implements Vie
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.activity_specialtopic_detail_share_iv:
+//
+                ShareSDK.initSDK(this);
+                OnekeyShare oks = new OnekeyShare();
+                //关闭sso授权 
+                // titleUrl是标题的网络链接，仅在人人网和QQ空间使用 
+                oks.disableSSOWhenAuthorize();
+                oks.setTitle("mob分享");
+                oks.setTitleUrl("http://sharesdk.cn");
+                // text是分享文本，所有平台都需要这个字段
+                oks.setText("我是分享文本");
+                // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数 //oks.setImagePath("/sdcard/test.jpg");
+                // 确保SDcard下面存在此张图片 // url仅在微信（包括好友和朋友圈）中使用 
+                oks.setImagePath("/sdcard/test.jpg");
+                oks.setUrl("http://sharesdk.cn");
+                // comment是我对这条分享的评论，仅在人人网和QQ空间使
+                oks.setComment("我是测试评论文本");
+                // site是分享此内容的网站名称，仅在QQ空间使用  
+                oks.setSite(getString(R.string.app_name));
+                // siteUrl是分享此内容的网站地址，仅在
+                oks.setSiteUrl("http://sharesdk.cn");
+                // 启动分享GUI
+                oks.show(this);
                 break;
             case R.id.activity_specialtopic_detail_close_iv:
                 finish();
