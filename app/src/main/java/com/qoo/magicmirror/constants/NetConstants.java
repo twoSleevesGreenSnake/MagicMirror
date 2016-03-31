b54cfbf524e5e7f051b6732d6f002b5e717382eb
薛景昊 <dllo@1208-27-xue.local>
2016-03-31 17:01:10 +0800

    浏览界面完成挺多

--- app/src/main/java/com/qoo/magicmirror/constants/NetConstants.java
+++ app/src/main/java/com/qoo/magicmirror/constants/NetConstants.java
@@ -3,7 +3,7 @@ package com.qoo.magicmirror.constants;
 /**
  * Created by dllo on 16/3/30.
  */
-public class NetConstants {
+public final class NetConstants {
     public static final String SERVIE_ADRESS = "http://api101.test.mirroreye.cn/";
 
     public static final String GOODS_TYPE = "index.php/products/goods_list";
