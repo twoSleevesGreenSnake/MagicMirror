1186ab7d77fd2b257981382bd69e29244da926d4
陈丰尧 <734481463@qq.com>
2016-04-01 10:27:14 +0800

    删了一些东西

--- app/src/main/java/com/qoo/magicmirror/view/SlowScrollImageView.java
+++ app/src/main/java/com/qoo/magicmirror/view/SlowScrollImageView.java
@@ -10,39 +10,5 @@ import android.widget.Scroller;
 /**
  * Created by dllo on 16/3/29.
  */
-public class SlowScrollImageView  extends ImageView{
-    private Scroller scroller;
-    private ViewGroup group;
-    public SlowScrollImageView(Context context) {
-        super(context);
-        initScroll(context);
+public class SlowScrollImageView{
     }
-
-    public SlowScrollImageView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        initScroll(context);
-    }
-
-    public SlowScrollImageView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        initScroll(context);
-    }
-
-    @Override
-    public void computeScroll() {
-        super.computeScroll();
-        if (scroller.computeScrollOffset()){
-            group = (ViewGroup) getParent();
-            group.scrollTo(scroller.getCurrX(), scroller.getCurrY());
-            invalidate();
-        }
-    }
-    private void initScroll(Context context){
-        scroller = new Scroller(context);
-        group = (ViewGroup) getParent();
-    }
-    public void starScroll(int x,int y){
-        scroller.startScroll(x/10,(int)getY(),-x/10,-(int)getY(),1000);
-        invalidate();
-    }
-}
